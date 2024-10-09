cd49563c9767c69025e6ab4fae12343458f617a7
eliezerBrasilian <eliezerassuncaocustodio@hotmail.com>
2024-10-08 21:23:13 -0300

    removing element inside of the list

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
- void main(){
-        var lista = new ListaEncadeada();
-        lista.adicionarNode("AB");
-        lista.adicionarNode("BC");
+import java.util.List;
+
+void main(){
+        var lista = new ListaEncadeada<>(List.of("AB","BC"));
         lista.adicionarNode("AC");
 
         lista.printNodes();
+
+        System.out.println("---------");
+
+        lista.remover("BC");
+        lista.printNodes();
+
 }
 
-class ListaEncadeada{
+class ListaEncadeada<String>{
     Node primeiro = null;
     Node ultimo = null;
     int tamanho;
+
+    public ListaEncadeada(){}
+
+    public ListaEncadeada(String[] nodes) {
+        for(int i = 0; i < nodes.length; i++){
+            adicionarNode(nodes[i]);
+        }
+    }
+
+    public ListaEncadeada(List<String> nodes) {
+        for(int i = 0; i < nodes.size(); i++){
+            adicionarNode(nodes.get(i));
+        }
+    }
 
     public void adicionarNode(String valor){
         var node =  new Node(valor);
@@ -25,6 +45,24 @@ class ListaEncadeada{
        tamanho ++;
     }
 
+               //AB,           BC,           AC
+               //anterior         //atual
+    public void remover(String procurado){
+        Node anterior = null;
+        var atual = primeiro;
+
+        for(int i = 0; i < tamanho; i ++){
+            //removendo o valor do meio
+            if(atual.valor == procurado){
+                anterior.proximo = atual.proximo;
+                tamanho --;
+                break;
+            }
+            anterior = atual;
+            atual = atual.proximo;
+        }
+    }
+
     public void printNodes(){
         var atual = primeiro;
         for(int i = 0; i < tamanho; i ++){
@@ -36,7 +74,7 @@ class ListaEncadeada{
     }
 }
 
-class Node{
+class Node<String>{
     String valor;
     Node proximo = null;
 
